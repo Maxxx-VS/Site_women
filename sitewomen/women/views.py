0e36b30bcddf236c5356318c75fa07c8dcf1089a
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-04-24 22:20:45 +0600

    №3 Настроены шаблоны и пути к ним

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+
 
 def index(request):
-    return HttpResponse("Страница приложения women")
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    return render(request, 'women/index.html')
+def about(request):
+    return render(request, 'women/about.html')
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id:{cat_id}</p>")
@@ -13,6 +20,10 @@ def categories_by_slug(request, cat_slug):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
 
 def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('sport', ))
+        # return redirect(uri)
+        return HttpResponsePermanentRedirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def page_not_found(request, exception):
