edf6068e73ff9d0612899e6a22bc4e680d9238bc
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-04-25 21:58:19 +0600

    №4 Настроены шаблоны и отображения из меню через цикл for

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,27 +4,59 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
+menu = [{'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить статью", 'url_name': 'add_page'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'},
+]
+
+data_db = [
+    {'id': 1, 'title': 'Анжделина Джоли', 'content': 'Биография Анджелины джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+
 def index(request):
     # t = render_to_string('women/index.html')
     # return HttpResponse(t)
-    return render(request, 'women/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
+
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id:{cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
-def categories_by_slug(request, cat_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        # return redirect(uri)
-        return HttpResponsePermanentRedirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def contact(request):
+    return HttpResponse('Обратная связь')
+def login(request):
+    return HttpResponse('Авторизация')
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+# def categories(request, cat_id):
+#     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id:{cat_id}</p>")
+#
+# def categories_by_slug(request, cat_slug):
+#     if request.POST:
+#         print(request.POST)
+#     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>")
+#
+# def archive(request, year):
+#     if year > 2023:
+#         uri = reverse('cats', args=('sport', ))
+#         # return redirect(uri)
+#         return HttpResponsePermanentRedirect(uri)
+#     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+#
