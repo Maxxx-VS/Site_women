6d0ed38138c7b0223e03d60ef08208f810c674ca
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-02 21:40:27 +0600

    №6 Создана БД через models

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True) # blank позволяет не передавать в content записи
+    time_create = models.DateTimeField(auto_now_add=True) # auto_now_add автоматически заполняет поле в момент появления записи
+    time_update = models.DateTimeField(auto_now=True) # auto_now каждый раз меняется в момент записи
+    is_published = models.BooleanField(default=True)
