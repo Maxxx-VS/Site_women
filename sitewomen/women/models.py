6db84004b6415ea2d77028b2c41fe4e61318ff6c
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-06 22:28:12 +0600

    №8 Lookups - запросы

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -6,3 +6,6 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True) # auto_now_add автоматически заполняет поле в момент появления записи
     time_update = models.DateTimeField(auto_now=True) # auto_now каждый раз меняется в момент записи
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
