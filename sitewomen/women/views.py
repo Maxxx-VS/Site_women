185cea56e2f1148b34cd605f49d5d526f87c7fac
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-07-06 22:47:55 +0600

    Пагинация насторена по 3шт

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -16,20 +17,22 @@ class WomenHome(DataMixin, ListView):
     context_object_name = 'posts'
     title_page = 'Главная страница'
     cat_selected = 0
+    paginate_by = 3
 
     def get_queryset(self):
         return Women.published.all().select_related('cat')
 
 def about(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            fp = UploadFiles(file=form.cleaned_data['file'])
-            fp.save()
-    else:
-        form = UploadFileForm()
+    contact_list = Women.published.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'women/about.html',
-                  {'title': 'О сайте', 'form': form})
+                  {'title': 'О сайте', 'page_obj': page_obj})
+
+
 class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
