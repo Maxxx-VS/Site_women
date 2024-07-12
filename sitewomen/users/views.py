e453f5e7b6bb088b3b89947b4e40d7eff1b88cf2
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-07-12 19:04:34 +0600

    Добавлена регистрация пользователей

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -5,12 +5,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
+
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title': "Авторизация"}
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # не заносим в БД
+            user.set_password(form.cleaned_data['password']) # производим шифрование пароля
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
 
     # def get_success_url(self):
     #     return reverse_lazy('home')
